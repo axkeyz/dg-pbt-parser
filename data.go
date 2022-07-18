25e4b46f16e7ba67bde0c639746c993c2d656be4
axkeyz <xahkun@gmail.com>
2022-07-18 22:45:46 +1200

    Format struct

--- data.go
+++ data.go
@@ -2,24 +2,29 @@
 // general management of PBT data
 package main
 
-import "strings"
+import (
+	"strings"
+)
 
 func Create200779Rows(worksheetRows [][]string) []PBTItem {
 	var pbtRows []PBTItem
+	customers := OpenConfigJSON("customers")
+	sales := OpenConfigJSON("sales")
 
 	for _, row := range worksheetRows[1:] {
+		customerRef := GetCustomerRef(row[2], row[23])
 		// Create new pbtItem from each row
 		pbtItem := PBTItem{
 			ConsignmentDate: row[4][0:10],
 			ManifestNum:     row[3],
 			Consignment:     row[0],
-			CustomerRef:     GetCustomerRef(row[2], row[23]),
+			CustomerRef:     customerRef,
 			ReceiverName:    strings.ToUpper(row[7]),
-			AreaTo:          strings.ToUpper(row[26]),
+			AreaTo:          GetRegion(row[18]),
 			TrackingNumber:  row[1],
 			Weight:          row[11],
 			Cubic:           row[12],
-			SortbyCode:      "",
+			SortbyCode:      GetSortbyCode(customerRef, row[7], customers, sales),
 		}
 
 		// add pbtItem to array of pbtDBRows
