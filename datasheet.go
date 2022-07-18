bf805a18c0b4dde452fda770ce63d8e504f65493
axkeyz <xahkun@gmail.com>
2022-07-19 10:45:15 +1200

    Update PBTOne runsheet extraction
    - Added uploads/ folder
    - Changed PBTOne runsheet detection to a separate function
        GetAllPBTOneRunsheets, that creates a slice of all
        runsheets that match the pattern uploads/*runsheet_exporting*.xlsx
    - Changed ExtractPBTOneRunsheet functon to open a given runsheet
        instead of setting the runsheet inside the function itself
    - Created CreateAll200779DBRows function which gets all PBTOne
        runsheets, formatting the data to a PBTItem struct and then
        writing them to database

--- datasheet.go
+++ datasheet.go
@@ -3,42 +3,60 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"path/filepath"
 
 	"github.com/xuri/excelize/v2"
 )
 
-// ExtractPBTONERunsheet extracts the data from the PBTONE
-// runsheet. This is usually a file that looks like
-// runsheet_exporting.xlsx
-func ExtractPBTONERunsheet() [][]string {
+// GetAllPBTOneRunsheets returns a []string of all files that
+// have a file name matching *runsheet_exporting*.xlsx
+func GetAllPBTOneRunsheets() []string {
 	// Search for a file that matches the runsheet file name
-	pattern := "*runsheet_exporting*.xls*"
-	var rows [][]string
-
+	pattern := "uploads/*runsheet_exporting*.xls*"
 	matches, err := filepath.Glob(pattern)
 
 	if err != nil {
 		// Display the error
 		fmt.Println(err)
-		return rows
 	}
 
-	runsheet := matches[0]
+	return matches
+}
+
+// ExtractPBTONERunsheet extracts the data from the named
+// PBTONE runsheet.
+func ExtractPBTONERunsheet(runsheet string) [][]string {
+	var rows [][]string
 
 	// Open the runsheet
 	f, err := excelize.OpenFile(runsheet)
 	if err != nil {
 		fmt.Println(err)
-		return rows
 	}
 	// Get all the rows in the Sheet1.
 	rows, err = f.GetRows("RunSheet_exporting")
 	if err != nil {
 		fmt.Println(err)
-		return rows
 	}
 
 	return rows
+}
+
+// CreateAll200779Rows creates new rows in the 200779 database
+// where the data is extracted from the available PBTOne spreadsheets.
+func CreateAll200779DBRows(database *sql.DB) {
+	// Get the PBT runsheets
+	runsheets := GetAllPBTOneRunsheets()
+
+	for _, runsheet := range runsheets {
+		// Create a PBT item row for each line in spreadsheet
+		pbtRows := Create200779Rows(ExtractPBTONERunsheet(runsheet))
+
+		// Create a new row in the database for each PBT item row
+		for _, row := range pbtRows {
+			NewDBRow(database, "pbt_200779", row)
+		}
+	}
 }
