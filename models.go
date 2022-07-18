c89bd23c397c596c28ef165302f0e45930d4d632
axkeyz <xahkun@gmail.com>
2022-07-19 11:47:00 +1200

    Add cost setter

--- models.go
+++ models.go
@@ -18,6 +18,26 @@ type PBTItem struct {
 	RuralDelivery   string
 	UnderTicket     string
 	Adjustment      string
+	Other           string
+	FFItems         string
 	FirstInvoice    string
 	LastInvoice     string
+}
+
+// SetPBTItemCost sets PBT a cost depending on the
+// given cost type.
+func SetPBTItemCost(cost string, costtype string,
+	item *PBTItem) {
+	switch costtype {
+	case "NOR":
+		item.ItemCost = cost
+	case "RUR":
+		item.RuralDelivery = cost
+	case "ADJ":
+		item.Adjustment = cost
+	case "UT":
+		item.UnderTicket = cost
+	case "CL":
+		item.Other = cost
+	}
 }
