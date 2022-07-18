fb7f2353683a57d84070e1a2394097a5f7e897e3
axkeyz <xahkun@gmail.com>
2022-07-18 12:40:31 +1200

    Isolate functions
    - Isolated functions so that functions will not be called in another
    function

--- main.go
+++ main.go
@@ -3,43 +3,12 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	_ "github.com/mattn/go-sqlite3"
 	"path/filepath"
-	"github.com/xuri/excelize/v2"
 	"strings"
+
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/xuri/excelize/v2"
 )
-
-// CreateDB creates a new "pbt" table in the database if it
-// does not exist already.
-func CreateDB(database *sql.DB) {
-	// Execute the query
-	_, err := database.Exec(`CREATE TABLE IF NOT EXISTS pbt_200779 (
-		id INTEGER PRIMARY KEY AUTOINCREMENT,
-		consignment_date DATE,
-		manifest_num TEXT NULL,
-		consignment TEXT NULL,
-		customer_ref TEXT NULL,
-		receivers_name TEXT NULL,
-		area_to TEXT NULL,
-		tracking_number TEXT UNIQUE,
-		weight FLOAT NULL,
-		cubic FLOAT NULL,
-		item_cost FLOAT NULL,
-		sortby_code TEXT NULL,
-		rural_delivery FLOAT NULL,
-		under_ticket FLOAT NULL,
-		adjustment FLOAT NULL,
-		first_invoice DATE,
-		last_invoice DATE
-	);`)
-
-	if err != nil {
-		// Display the error
-		fmt.Println(err)
-	}
-
-	return
-}
 
 // ExtractPBTONERunsheet extracts the data from the PBTONE
 // runsheet. This is usually a file that looks like
@@ -61,58 +30,62 @@ func ExtractPBTONERunsheet() [][]string {
 
 	// Open the runsheet
 	f, err := excelize.OpenFile(runsheet)
-    if err != nil {
-        fmt.Println(err)
-        return rows
-    }
-    // Get all the rows in the Sheet1.
-    rows, err = f.GetRows("RunSheet_exporting")
-    if err != nil {
-        fmt.Println(err)
-        return rows
-    }
+	if err != nil {
+		fmt.Println(err)
+		return rows
+	}
+	// Get all the rows in the Sheet1.
+	rows, err = f.GetRows("RunSheet_exporting")
+	if err != nil {
+		fmt.Println(err)
+		return rows
+	}
 
-    return rows
+	return rows
 }
 
-func Create200779Rows() {
-	var consignment_date, manifest_num, consignment string
-	var customer_ref, receivers_name, area_to string
-	var weight, cubic string
-	// var under_ticket, adjustment float64
-	var tracking_number, sortby_code string
+func Create200779Rows(worksheetRows [][]string) []PBTItem {
+	var pbtRows []PBTItem
 
-	rows := ExtractPBTONERunsheet()
-
-	for _, row := range rows[1:] {
-		consignment = row[0]
-		tracking_number = row[1]
-		customer_ref = row[2]
-		if row[23] != "" {
-			customer_ref = customer_ref + " (" + row[23] + ")"
+	for _, row := range worksheetRows[1:] {
+		// Create new pbtDBItem
+		pbtItem := PBTItem{
+			ConsignmentDate: row[4][0:10],
+			ManifestNum:     row[3],
+			Consignment:     row[0],
+			CustomerRef:     GetCustomerRef(row[2], row[23]),
+			ReceiverName:    strings.ToUpper(row[7]),
+			AreaTo:          strings.ToUpper(row[26]),
+			TrackingNumber:  row[1],
+			Weight:          row[11],
+			Cubic:           row[12],
+			SortbyCode:      "",
 		}
-		manifest_num = row[3]
-		consignment_date = row[4][0:10]
-		receivers_name = strings.ToUpper(row[7])
-		weight = row[11]
-		cubic = row[12]
-		area_to = strings.ToUpper(row[26])
-		sortby_code = ""
 
-		fmt.Println(consignment_date, manifest_num, consignment,
-		customer_ref, receivers_name, area_to, weight, cubic,
-		tracking_number, sortby_code)
+		// add pbtItem to array of pbtDBRows
+		pbtRows = append(pbtRows, pbtItem)
 	}
+
+	return pbtRows
+}
+
+// GetCustomerRef combines the main reference (mainRef) with the
+// secondary reference (subRef) if applicable.
+func GetCustomerRef(mainRef string, subRef string) string {
+	if subRef != "" {
+		// Combine main reference with secondary reference
+		mainRef = mainRef + " (" + subRef + ")"
+	}
+	// Return output
+	return mainRef
 }
 
 func main() {
 	database, _ := sql.Open("sqlite3", "./dgpbt.db")
 
-	CreateDB(database)
+	CreateDB(database, "pbt_200779")
 
-	Create200779Rows()
+	Create200779Rows(ExtractPBTONERunsheet())
 
 	fmt.Println("Done")
-
-	fmt.Println("This won't run")
 }
