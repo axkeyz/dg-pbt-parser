274a76d4cd1268cb7c51115297df64d24a64455f
axkeyz <xahkun@gmail.com>
2022-07-18 12:39:04 +1200

    Update models
    - Added fields to the PBTItem struct to better represent all
        requirements of a row of PBT data (one row per item)

--- models.go
+++ models.go
@@ -1,5 +1,23 @@
 package main
 
-type PBT200779Row struct {
+// PBTItem represents a row of PBT data (one row per freight
+// item).
+type PBTItem struct {
+	ID              string
 	ConsignmentDate string
+	ManifestNum     string
+	Consignment     string
+	CustomerRef     string
+	ReceiverName    string
+	AreaTo          string
+	TrackingNumber  string
+	Weight          string
+	Cubic           string
+	ItemCost        string
+	SortbyCode      string
+	RuralDelivery   string
+	UnderTicket     string
+	Adjustment      string
+	FirstInvoice    string
+	LastInvoice     string
 }
